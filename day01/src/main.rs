480f3510f66245bd405db77c009b6221283dc706
J.M. Thiessen <jacob@x0rz3q.com>
2020-12-01 08:09:13 +0100

    Add day 1 - It's ugly, but it works

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,36 @@
+fn silver(input: Vec<i64>) -> i64 {
+	for x in input.clone() {
+		for y in input.clone() {
+			if x + y == 2020 {
+				return x * y;
+			}
+		}
+	}
+
+	0
+}
+
+fn gold(input: Vec<i64>) -> i64 {
+	for x in input.clone() {
+		for y in input.clone() {
+			for z in input.clone() {
+				if x + y + z == 2020 {
+					return x * y * z;
+				}
+			}
+		}
+	}
+
+	0
+}
+
 fn main() {
-	let input: Vec<String> = include_str!("input")
+	let input: Vec<i64> = include_str!("input")
 		.trim()
 		.split('\n')
+		.map(|num| num.parse::<i64>().unwrap())
 		.collect();
+
+	println!("Silver: {}", silver(input.clone()));
+	println!("Gold: {}", gold(input.clone()));
 }
