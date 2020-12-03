98b6140c2c3935bacce8497ae87545720683a91b
J.M. Thiessen <jacob@x0rz3q.com>
2020-12-03 21:32:16 +0100

    Add new skeleton code

--- skeleton/main.rs
+++ skeleton/main.rs
@@ -1,5 +1,37 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::cmp;
+
+#[derive(Hash, Clone, Debug, Copy)]
+struct Coordinate {
+	x: i64,
+	y: i64
+}
+
+impl Coordinate {
+	fn new(x: i64, y: i64) -> Coordinate {
+		Coordinate{x, y}
+	}
+}
+
+impl Eq for Coordinate {}
+
+impl PartialEq for Coordinate {
+	fn eq(&self, other: &Self) -> bool {
+		self.x == other.x && self.y == other.y
+	}
+}
+
+fn silver(input: Vec<TYPE>) {
+
+}
+
+fn gold(input: Vec<TYPE>) {
+
+}
+
 fn main() {
-	let input: Vec<String> = include_str!("input")
+	let input: Vec<TYPE> = include_str!("input")
 		.trim()
 		.split('\n')
 		.collect();
