f8c99874f4f91cb8be474c9f66b528dff619fa66
J.M. Thiessen <jacob@x0rz3q.com>
2020-12-04 10:55:19 +0100

    Rewrite to use regexes instead of using custom parsing

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -15,8 +15,6 @@ fn silver(input: &Vec<String>) -> usize {
 
 fn gold(input: &Vec<String>) -> usize {
     let constraints = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-    let eyes = vec!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
-    let hcl_re = Regex::new(r"#[0-9a-f]{6}").unwrap();
 
     let input: Vec<String> = input
         .iter()
@@ -24,7 +22,23 @@ fn gold(input: &Vec<String>) -> usize {
         .map(|x| x.to_string())
         .collect();
 
-    let mut valid = 0;
+    let mut comparitor: HashMap<&str, Regex> = HashMap::new();
+    comparitor.insert(
+        "byr",
+        Regex::new(r"^19[2-8][0-9]|199[0-9]|200[0-2]$").unwrap(),
+    );
+    comparitor.insert("iyr", Regex::new(r"^(201[0-9]|2020)$").unwrap());
+    comparitor.insert("eyr", Regex::new(r"^(202[0-9]|2030)$").unwrap());
+    comparitor.insert(
+        "hgt",
+        Regex::new(r"^((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)$").unwrap(),
+    );
+    comparitor.insert("hcl", Regex::new(r"^#[0-9a-f]{6}$").unwrap());
+    comparitor.insert("ecl", Regex::new(r"^amb|blu|brn|gry|grn|hzl|oth$").unwrap());
+    comparitor.insert("pid", Regex::new(r"^[0-9]{9}$").unwrap());
+    comparitor.insert("cid", Regex::new(r"").unwrap());
+
+    let mut count = 0;
     for entry in input {
         let map = entry
             .split_whitespace()
@@ -32,73 +46,22 @@ fn gold(input: &Vec<String>) -> usize {
             .tuples()
             .collect::<HashMap<_, _>>();
 
-        let byr = map.get("byr").unwrap().parse::<u64>();
-        let iyr = map.get("iyr").unwrap().parse::<u64>();
-        let eyr = map.get("eyr").unwrap().parse::<u64>();
+        let mut valid = true;
+        for (key, value) in map.iter() {
+            valid = valid && comparitor.get(key).unwrap().is_match(value);
 
-        if byr.is_err() || iyr.is_err() || eyr.is_err() {
-            println!("{}", "Invalid byr/iyr/eyr".red());
-            continue;
+            if !valid {
+                break;
+            }
         }
 
-        let byr = byr.unwrap();
-        let iyr = iyr.unwrap();
-        let eyr = eyr.unwrap();
-
-        if !((1920 <= byr && byr <= 2002)
-            && (2010 <= iyr && iyr <= 2020)
-            && (2020 <= eyr && eyr <= 2030))
-        {
-            println!("{}", "Invalid byr/iyr/eyr value".red());
-            continue;
+        if valid {
+            println!("{}", entry);
+            count += 1;
         }
-
-        let hgt = map.get("hgt").unwrap();
-        if hgt.matches("in").count() == 0 && hgt.matches("cm").count() == 0 {
-            println!("{}", "Invalid hgt format".red());
-            continue;
-        }
-
-        let is_cm = hgt.matches("cm").count() == 1;
-        let hgt = hgt.replace("in", "").replace("cm", "");
-        let hgt = hgt.parse::<u64>();
-
-        if hgt.is_err() {
-            println!("{}", "Invalid hgt integer".red());
-            continue;
-        }
-
-        let hgt = hgt.unwrap();
-        if !((is_cm && 150 <= hgt && hgt <= 193) || (!is_cm && 59 <= hgt && hgt <= 76)) {
-            println!(
-                "{}",
-                format!("Invalid hgt value {} with is_cm {}", hgt, is_cm).red()
-            );
-            continue;
-        }
-
-        let hcl = map.get("hcl").unwrap();
-        if !hcl_re.is_match(hcl) {
-            println!("{}", format!("Invalid hcl value {}", hcl).red());
-            continue;
-        }
-
-        let pid = map.get("pid").unwrap();
-        if pid.len() != 9 || pid.parse::<u64>().is_err() {
-            println!("{}", format!("Invalid pid {}", pid).red());
-            continue;
-        }
-
-        let ecl = map.get("ecl").unwrap();
-        if !eyes.iter().any(|x| x == ecl) {
-            println!("{}", format!("Invalid ecl {}", ecl).red());
-            continue;
-        }
-
-        valid += 1;
     }
 
-    valid
+    count
 }
 
 fn main() {
