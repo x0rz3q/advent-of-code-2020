e567fbe2a491e73fcd84259a95a94221fd271322
J.M. Thiessen <jacob@x0rz3q.com>
2020-12-06 11:34:59 +0100

    Adjust setup script to include build script additions

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import sys
 
 if __name__ == '__main__':
     # Create the repository
@@ -9,6 +10,9 @@ if __name__ == '__main__':
 
     if len(day) == 1:
         day = '0' + day
+
+    if len(sys.argv) > 1:
+        day = sys.argv[1]
 
     os.system(f"cargo new day{day}")
     # copy the skeleton file
@@ -27,4 +31,28 @@ if __name__ == '__main__':
     lines.append('itertools = "0.9.0"')
 
     with open(f"day{day}/Cargo.toml", 'w') as fp:
+        fp.write('\n'.join(lines))
+
+    with open('.gitlab-ci.yml', 'r') as fp:
+        lines = [line.replace('\n', '') for line in fp.readlines()]
+
+    content = [
+            '',
+            f"build:day-{day}:",
+            '  stage: build',
+            '  script:',
+            f"    - cd day{day}",
+            '    - cargo build --release',
+            '',
+            f"run:day-{day}:",
+            '  stage: test',
+            '  script:',
+            f"    - cd day{day}",
+            '    - cargo run --release',
+            ''
+    ]
+
+    lines = lines + content
+
+    with open('.gitlab-ci.yml', 'w') as fp:
         fp.write('\n'.join(lines))
