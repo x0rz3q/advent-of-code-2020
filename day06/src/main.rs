a53d981fd1ecd6d2e10d2529f7c339bc2b296208
J.M. Thiessen <jacob@x0rz3q.com>
2020-12-06 09:04:14 +0100

    Add day06

---
+++ day06/src/main.rs
@@ -0,0 +1,39 @@
+use std::{collections::HashSet, time::Instant};
+
+fn silver(input: &Vec<String>) -> usize {
+	input
+		.iter()
+		.map(|x| x.replace("\n", "").chars().collect::<HashSet<char>>().len())
+		.sum()
+}
+
+fn gold(input: &Vec<String>) -> usize {
+	input
+		.iter()
+		.map(|x| {
+			let len = x.matches("\n").count() + 1;
+
+			x.replace("\n", "")
+				.chars()
+				.collect::<HashSet<char>>()
+				.iter()
+				.filter(|c| x.matches(&c.to_string()).count() == len)
+				.count()
+		})
+		.sum()
+}
+
+fn main() {
+	let now = Instant::now();
+
+	let input: Vec<String> = include_str!("input")
+		.trim()
+		.split("\n\n")
+		.map(|x| x.to_string())
+		.collect();
+
+	println!("Silver: {}", silver(&input));
+	println!("Silver: {}", gold(&input));
+
+	println!("Time: {}ms", now.elapsed().as_millis());
+}
